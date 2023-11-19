7cff0e63886d5b9dbf2dfd3c6a38e8212e591b86
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-19 12:03:06 +0000

    Create Post model

---
+++ models/post.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    text: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+    date_posted: { type: Date, default: Date.now },
+    visible: { type: Boolean, required: true, default: true },
+});
+
+PostSchema.virtual("date_posted_formatted").get(function () {
+    return this.date_posted
+        ? DateTime.fromJSDate(this.date_posted).toLocaleString(
+              DateTime.DATETIME_SHORT_WITH_SECONDS
+          )
+        : null;
+});
+
+export default mongoose.model("Post", PostSchema);
