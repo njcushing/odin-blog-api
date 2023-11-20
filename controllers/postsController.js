cdef463e48ad0c4c67bc2e611c64e8fd9eef7382
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-20 20:19:58 +0000

    Write /posts POST request controller method

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,7 +1,32 @@
 import mongoose from "mongoose";
 import asyncHandler from "express-async-handler";
+import { body, validationResult } from "express-validator";
 
 import Post from "../models/post.js";
+
+const validatePostId = (postId) => {
+    if (!mongoose.Types.ObjectId.isValid(postId)) {
+        res.send("Provided resource id is invalid.");
+    }
+};
+
+const validateFields = [
+    body("title", "'title' field (string) must not be empty")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("text", "'text' field (string) must not be empty")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("visible")
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage("'visible' field (boolean) must not be empty")
+        .escape()
+        .isBoolean()
+        .withMessage("'visible' field must be a boolean"),
+];
 
 export const postsGet = asyncHandler(async (req, res, next) => {
     const posts = await Post.find().exec();
@@ -14,9 +39,7 @@ export const postsGet = asyncHandler(async (req, res, next) => {
 
 export const postGet = asyncHandler(async (req, res, next) => {
     const postId = req.params.postId;
-    if (!mongoose.Types.ObjectId.isValid(postId)) {
-        res.send("Provided resource id is invalid.");
-    }
+    validatePostId(postId);
     const posts = await Post.findById(postId).exec();
     if (posts !== null) {
         res.json(posts);
@@ -25,9 +48,31 @@ export const postGet = asyncHandler(async (req, res, next) => {
     }
 });
 
-export const postCreate = asyncHandler(async (req, res, next) => {
-    res.send("Post CREATE request");
-});
+export const postCreate = [
+    ...validateFields,
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        const post = new Post({
+            title: req.body.title,
+            text: req.body.text,
+            comments: [],
+            date_posted: Date.now(),
+            visible: req.body.visible,
+        });
+        if (!errors.isEmpty()) {
+            const reducedErrorArray = [];
+            errors.array().forEach((error) => {
+                reducedErrorArray.push(error.msg);
+            });
+            res.send(
+                `Unable to create new post: ${reducedErrorArray.join(", ")}`
+            );
+        } else {
+            await post.save();
+            res.send(`New post successfully created. Post Id: ${post._id}`);
+        }
+    }),
+];
 
 export const postUpdate = asyncHandler(async (req, res, next) => {
     const postId = req.params.postId;
