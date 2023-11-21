ea49da4c0212c3175fcf40d52601e23ddf86e8d9
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-21 19:12:44 +0000

    Write helper functions for comment controller methods
    Including function to validate provided postId and commentId, function
    to validate req.body parameters or form fields and a function to respond
    with send validation errors to the client.

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,4 +1,48 @@
+import mongoose from "mongoose";
 import asyncHandler from "express-async-handler";
+import { body, validationResult } from "express-validator";
+
+import Comment from "../models/comment.js";
+import Post from "../models/post.js";
+
+export const validateDocumentIds = (res, postId, commentId) => {
+    const validPostId = mongoose.Types.ObjectId.isValid(postId);
+    const validCommentId = mongoose.Types.ObjectId.isValid(commentId);
+    if (!validPostId || !validCommentId) {
+        res.send(
+            `${
+                validPostId && validCommentId
+                    ? `Provided post id and comment id are both invalid.`
+                    : validPostId
+                    ? "Provided post id is invalid."
+                    : "Provided comment id is invalid."
+            }`
+        );
+    }
+};
+
+const validateFields = [
+    body("first_name", "'first_name' field (string) must not be empty")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("last_name", "'last_name' field (string) must not be empty")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("text", "'text' field (string) must not be empty")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+];
+
+const sendValidationErrors = (res, errorsArray) => {
+    const reducedErrorArray = [];
+    errorsArray.forEach((error) => {
+        reducedErrorArray.push(error.msg);
+    });
+    res.send(`Unable to create new comment: ${reducedErrorArray.join(", ")}`);
+};
 
 export const commentsGet = asyncHandler(async (req, res, next) => {
     res.send("Comments GET request");
