73def34266e65c8b7969dd1a4ac6ef45200b8dfb
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-23 12:45:23 +0000

    Respond with token on successful login
    Written the part of the /login route POST controller method that queries
    the database to find the user specified in the request and validates the
    hashed password. On successful login, respond with token to the client

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,73 @@
 import asyncHandler from "express-async-handler";
 import { body, validationResult } from "express-validator";
+import createError from "http-errors";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
-export const loginPost = asyncHandler(async (req, res, next) => {
-    res.send("/login POST request");
-});
+import User from "../models/user.js";
+
+const compileValidationErrors = (errorsArray) => {
+    const reducedErrorArray = [];
+    errorsArray.forEach((error) => {
+        reducedErrorArray.push(error.msg);
+    });
+    return reducedErrorArray.join(", ");
+};
+
+export const loginPost = [
+    body("username", "'username' field must be present and filled in request")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("password", "'password' field must be present and filled in request")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const errorString = compileValidationErrors(errors.array());
+            return next(
+                createError(401, `Invalid login attempt: ${errorString}`)
+            );
+        } else {
+            const { username, password } = req.body;
+            const user = await User.findOne({ username: username });
+            if (!user) {
+                return next(
+                    createError(
+                        401,
+                        `Invalid login attempt: Incorrect user credentials.`
+                    )
+                );
+            }
+            const match = await bcrypt.compare(password, user.password);
+            if (!match) {
+                return next(
+                    createError(
+                        401,
+                        `Invalid login attempt: Incorrect user credentials.`
+                    )
+                );
+            }
+            jwt.sign(
+                { user: req.user },
+                process.env.AUTH_SECRET_KEY,
+                { expiresIn: "600s" },
+                (err, token) => {
+                    if (err) {
+                        return next(
+                            createError(401, `Invalid login attempt: ${err}`)
+                        );
+                    } else {
+                        res.status(200).send({
+                            status: 200,
+                            message: "Successfully logged in!",
+                            data: { token: token },
+                        });
+                    }
+                }
+            );
+        }
+    }),
+];
