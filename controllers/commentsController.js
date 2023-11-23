1a47fdcab3ae421388e9c4b3e55481cf9773fd62
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-23 11:06:30 +0000

    Add proper error handling to /comments routes for invalid mongoDB _ids

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -5,32 +5,39 @@ import { body, validationResult } from "express-validator";
 import Comment from "../models/comment.js";
 import Post from "../models/post.js";
 
-const validateCommentId = (res, commentId) => {
-    if (!mongoose.Types.ObjectId.isValid(commentId)) {
-        res.send("Provided comment id is invalid.");
-    }
-};
-
-const validatePostId = (res, postId) => {
+const validatePostId = (next, postId) => {
     if (!mongoose.Types.ObjectId.isValid(postId)) {
-        res.send("Provided post id is invalid.");
+        return next(createError(400, `Provided postId: ${postId} is invalid.`));
     }
+    return true;
 };
 
-export const validateDocumentIds = (res, postId, commentId) => {
+export const validateDocumentIds = (next, postId, commentId) => {
     const validPostId = mongoose.Types.ObjectId.isValid(postId);
     const validCommentId = mongoose.Types.ObjectId.isValid(commentId);
     if (!validPostId || !validCommentId) {
-        res.send(
-            `${
-                !validPostId && !validCommentId
-                    ? `Provided post id and comment id are both invalid.`
-                    : !validPostId
-                    ? "Provided post id is invalid."
-                    : "Provided comment id is invalid."
-            }`
+        return next(
+            createError(
+                400,
+                `${
+                    !validPostId && !validCommentId
+                        ? `Provided postId: ${postId} and commentId: ${commentId} are both invalid.`
+                        : !validPostId
+                        ? `Provided postId: ${postId} is invalid.`
+                        : `Provided commentId: ${commentId} is invalid.`
+                }`
+            )
         );
     }
+    return true;
+};
+
+const commentNotFound = (commentId) => {
+    return createError(404, `Specified comment not found at: ${commentId}.`);
+};
+
+const postNotFound = (postId) => {
+    return createError(404, `Specified post not found at: ${postId}.`);
 };
 
 const validateFields = [
@@ -58,41 +65,37 @@ const sendValidationErrors = (res, errorsArray) => {
 
 export const commentsGet = asyncHandler(async (req, res, next) => {
     const postId = req.params.postId;
-    validatePostId(res, postId);
+    if (!validatePostId(next, postId)) return;
     const post = await Post.findById(postId).populate("comments").exec();
-    if (post === null) {
-        res.send(`Specified post not found at: ${postId}.`);
-    } else {
-        try {
-            const comments = post.comments;
-            res.json(comments);
-        } catch (err) {
-            res.send(
-                `Post found at: ${postId}, but 'comments' field was not found on the returned document.`
-            );
-        }
+    if (post === null) return next(postNotFound(postId));
+
+    try {
+        const comments = post.comments;
+        res.json(comments);
+    } catch (err) {
+        res.send(
+            `Post found at: ${postId}, but 'comments' field was not found on the returned document.`
+        );
     }
 });
 
 export const commentGet = asyncHandler(async (req, res, next) => {
     const postId = req.params.postId;
     const commentId = req.params.commentId;
-    validateDocumentIds(res, postId, commentId);
-    const post = await Post.findById(postId);
-    const comment = await Comment.findById(commentId);
-    if (post === null) {
-        res.send(`Specified post not found at: ${postId}.`);
-    }
-    if (comment === null) {
-        res.send(`Specified comment not found at: ${commentId}.`);
+    if (!validateDocumentIds(next, postId, commentId)) return;
+    const [post, comment] = await Promise.all([
+        Post.findById(postId),
+        Comment.findById(commentId),
+    ]);
+    if (post === null) return next(postNotFound(postId));
+    if (comment === null) return next(commentNotFound(commentId));
+
+    if (comment.parent_post.toString() !== postId) {
+        res.send(
+            `Comment exists, but it is not in reply to the specified post.`
+        );
     } else {
-        if (comment.parent_post.toString() !== postId) {
-            res.send(
-                `Comment exists, but it is not in reply to the specified post.`
-            );
-        } else {
-            res.json(comment);
-        }
+        res.json(comment);
     }
 });
 
@@ -100,39 +103,37 @@ export const commentCreate = [
     ...validateFields,
     asyncHandler(async (req, res, next) => {
         const postId = req.params.postId;
-        validatePostId(res, postId);
-        const post = await Post.findById(postId);
-        if (post === null) {
-            res.send(`Specified post not found at: ${postId}.`);
+        if (!validatePostId(next, postId)) return;
+        const post = await Post.findById(postId).populate("comments").exec();
+        if (post === null) return next(postNotFound(postId));
+
+        const newCommentId = new mongoose.Types.ObjectId();
+        const errors = validationResult(req);
+        const comment = new Comment({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            text: req.body.text,
+            date_posted: Date.now(),
+            replies: [],
+            parent_post: postId,
+            parent_comment: null,
+            _id: newCommentId,
+        });
+        if (!errors.isEmpty()) {
+            sendValidationErrors(res, errors.array());
         } else {
-            const newCommentId = new mongoose.Types.ObjectId();
-            const errors = validationResult(req);
-            const comment = new Comment({
-                first_name: req.body.first_name,
-                last_name: req.body.last_name,
-                text: req.body.text,
-                date_posted: Date.now(),
-                replies: [],
-                parent_post: postId,
-                parent_comment: null,
-                _id: newCommentId,
+            const updatedParentPost = await Post.findByIdAndUpdate(postId, {
+                $push: { comments: newCommentId },
             });
-            if (!errors.isEmpty()) {
-                sendValidationErrors(res, errors.array());
+            if (updatedParentPost === null) {
+                res.send(
+                    `Specified post not found at: ${postId}. Comment was not saved.`
+                );
             } else {
-                const updatedParentPost = await Post.findByIdAndUpdate(postId, {
-                    $push: { comments: newCommentId },
-                });
-                if (updatedParentPost === null) {
-                    res.send(
-                        `Specified post not found at: ${postId}. Comment was not saved.`
-                    );
-                } else {
-                    await comment.save();
-                    res.send(
-                        `New comment successfully created. Comment Id: ${newCommentId}`
-                    );
-                }
+                await comment.save();
+                res.send(
+                    `New comment successfully created. Comment Id: ${newCommentId}`
+                );
             }
         }
     }),
@@ -143,60 +144,59 @@ export const replyCreate = [
     asyncHandler(async (req, res, next) => {
         const postId = req.params.postId;
         const parentCommentId = req.params.commentId;
-        validateDocumentIds(res, postId, parentCommentId);
+        if (!validateDocumentIds(next, postId, parentCommentId)) return;
         const [post, parentComment] = await Promise.all([
             Post.findById(postId),
             Comment.findById(parentCommentId),
         ]);
-        if (post === null) {
-            res.send(`Specified post not found at: ${postId}.`);
-        } else if (parentComment === null) {
-            res.send(
-                `Specified comment to reply to not found at: ${parentCommentId}.`
-            );
-        } else if (parentComment.parent_post.toString() !== postId) {
-            res.send(
+        if (post === null) return next(postNotFound(postId));
+        if (parentComment === null) {
+            return next(commentNotFound(parentCommentId));
+        }
+        if (parentComment.parent_post.toString() !== postId) {
+            return res.send(
                 `Comment to reply to exists, but it is not in reply to the specified post.`
             );
+        }
+
+        const newCommentId = new mongoose.Types.ObjectId();
+        const errors = validationResult(req);
+        const comment = new Comment({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            text: req.body.text,
+            date_posted: Date.now(),
+            replies: [],
+            parent_post: postId,
+            parent_comment: parentCommentId,
+            _id: newCommentId,
+        });
+        if (!errors.isEmpty()) {
+            sendValidationErrors(res, errors.array());
         } else {
-            const newCommentId = new mongoose.Types.ObjectId();
-            const errors = validationResult(req);
-            const comment = new Comment({
-                first_name: req.body.first_name,
-                last_name: req.body.last_name,
-                text: req.body.text,
-                date_posted: Date.now(),
-                replies: [],
-                parent_post: postId,
-                parent_comment: parentCommentId,
-                _id: newCommentId,
-            });
-            if (!errors.isEmpty()) {
-                sendValidationErrors(res, errors.array());
+            const [updatedParentPost, updatedParentComment] = await Promise.all(
+                [
+                    Post.findByIdAndUpdate(postId, {
+                        $push: { comments: newCommentId },
+                    }),
+                    Comment.findByIdAndUpdate(parentCommentId, {
+                        $push: { replies: newCommentId },
+                    }),
+                ]
+            );
+            if (updatedParentPost === null) {
+                res.send(
+                    `Specified post not found at: ${postId}. Comment was not saved.`
+                );
+            } else if (updatedParentComment === null) {
+                res.send(
+                    `Specified comment to reply to not found at: ${parentCommentId}. Comment was not saved.`
+                );
             } else {
-                const [updatedParentPost, updatedParentComment] =
-                    await Promise.all([
-                        Post.findByIdAndUpdate(postId, {
-                            $push: { comments: newCommentId },
-                        }),
-                        Comment.findByIdAndUpdate(parentCommentId, {
-                            $push: { replies: newCommentId },
-                        }),
-                    ]);
-                if (updatedParentPost === null) {
-                    res.send(
-                        `Specified post not found at: ${postId}. Comment was not saved.`
-                    );
-                } else if (updatedParentComment === null) {
-                    res.send(
-                        `Specified comment to reply to not found at: ${parentCommentId}. Comment was not saved.`
-                    );
-                } else {
-                    await comment.save();
-                    res.send(
-                        `New comment successfully created. Comment Id: ${newCommentId}`
-                    );
-                }
+                await comment.save();
+                res.send(
+                    `New comment successfully created. Comment Id: ${newCommentId}`
+                );
             }
         }
     }),
@@ -207,45 +207,41 @@ export const commentUpdate = [
     asyncHandler(async (req, res, next) => {
         const postId = req.params.postId;
         const commentId = req.params.commentId;
-        validateDocumentIds(res, postId, commentId);
+        if (!validateDocumentIds(next, postId, commentId)) return;
         const [post, comment] = await Promise.all([
             Post.findById(postId),
             Comment.findById(commentId),
         ]);
-        if (post === null) {
-            res.send(`Specified post not found at: ${postId}.`);
-        } else if (comment === null) {
-            res.send(
-                `Specified comment to reply to not found at: ${commentId}.`
-            );
-        } else if (comment.parent_post.toString() !== postId) {
+        if (post === null) return next(postNotFound(postId));
+        if (comment === null) return next(commentNotFound(commentId));
+        if (comment.parent_post.toString() !== postId) {
             res.send(
                 `Comment to update exists, but it is not in reply to the specified post.`
             );
+        }
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            sendValidationErrors(res, errors.array());
         } else {
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                sendValidationErrors(res, errors.array());
-            } else {
-                const updatedComment = await Comment.findByIdAndUpdate(
-                    commentId,
-                    {
-                        $set: {
-                            first_name: req.body.first_name,
-                            last_name: req.body.last_name,
-                            text: req.body.text,
-                            date_last_updated: Date.now(),
-                        },
+            const updatedComment = await Comment.findByIdAndUpdate(
+                commentId,
+                {
+                    $set: {
+                        first_name: req.body.first_name,
+                        last_name: req.body.last_name,
+                        text: req.body.text,
+                        date_last_updated: Date.now(),
                     },
-                    { new: true }
-                );
-                if (updatedComment === null) {
-                    res.send(`Specified post not found at: ${commentId}.`);
-                }
-                res.send(
-                    `Comment successfully updated at: ${commentId}. New comment details: ${updatedComment}`
-                );
+                },
+                { new: true }
+            );
+            if (updatedComment === null) {
+                res.send(`Specified post not found at: ${commentId}.`);
             }
+            res.send(
+                `Comment successfully updated at: ${commentId}. New comment details: ${updatedComment}`
+            );
         }
     }),
 ];
@@ -253,58 +249,58 @@ export const commentUpdate = [
 export const commentDelete = asyncHandler(async (req, res, next) => {
     const postId = req.params.postId;
     const commentId = req.params.commentId;
-    validateDocumentIds(res, postId, commentId);
+    if (!validateDocumentIds(next, postId, commentId)) return;
     const [post, comment] = await Promise.all([
         Post.findById(postId),
         Comment.findById(commentId),
     ]);
-    if (post === null) {
-        res.send(`Specified post not found at: ${postId}.`);
-    } else if (comment === null) {
-        res.send(`Specified comment to reply to not found at: ${commentId}.`);
-    } else if (comment.parent_post.toString() !== postId) {
+    if (post === null) return next(postNotFound(postId));
+    if (comment === null) return next(commentNotFound(commentId));
+    if (comment.parent_post.toString() !== postId) {
         res.send(
-            `Comment to delete exists, but it is not in reply to the specified post.`
+            `Comment to update exists, but it is not in reply to the specified post.`
         );
-    } else {
-        // Resursively delete all the replies to the comment
-        const commentsToDeleteSet = new Set();
-        const commentsToDeleteArray = [];
-        const findRepliesToComment = async (commentId) => {
-            if (commentsToDeleteSet.has(commentId)) return;
-            const comment = await Comment.findById(commentId);
-            if (comment === null) return;
-            commentsToDeleteSet.add(commentId);
-            commentsToDeleteArray.push(commentId);
-            await Promise.all(
-                comment.replies.map(async (reply) => {
-                    await findRepliesToComment(reply._id);
-                })
-            );
-        };
+    }
+
+    // Resursively delete all the replies to the comment
+    const commentsToDeleteSet = new Set();
+    const commentsToDeleteArray = [];
+    const findRepliesToComment = async (commentId) => {
+        if (commentsToDeleteSet.has(commentId)) return;
+        // Making repeated db queries instead of one more broad query is likely
+        // horribly performant when scaled up, but it works for our small site
+        const comment = await Comment.findById(commentId);
+        if (comment === null) return;
+        commentsToDeleteSet.add(commentId);
+        commentsToDeleteArray.push(commentId);
         await Promise.all(
-            comment.replies.map(async (comment) => {
-                await findRepliesToComment(comment._id);
+            comment.replies.map(async (reply) => {
+                await findRepliesToComment(reply._id);
             })
         );
+    };
+    await Promise.all(
+        comment.replies.map(async (comment) => {
+            await findRepliesToComment(comment._id);
+        })
+    );
 
-        const deletedComments = await Comment.deleteMany({
-            _id: { $in: commentsToDeleteArray },
-        });
-        const deletedCount = deletedComments.deletedCount + 1;
+    const deletedComments = await Comment.deleteMany({
+        _id: { $in: commentsToDeleteArray },
+    });
+    const deletedCount = deletedComments.deletedCount + 1;
 
-        const deletedComment = await Comment.findByIdAndDelete(commentId);
+    const deletedComment = await Comment.findByIdAndDelete(commentId);
 
-        res.send(`
-            ${
-                deletedComment === null
-                    ? `Specified comment not found at: ${commentId}. ${deletedCount} comments deleted${
-                          deletedCount === 0
-                              ? `.`
-                              : ` (comment was found in initial query but may have been deleted by another source prior to deletion by this process).`
-                      }`
-                    : `comment successfully deleted at: ${commentId}. ${deletedCount} comments deleted.`
-            }
-        `);
-    }
+    res.send(`
+        ${
+            deletedComment === null
+                ? `Specified comment not found at: ${commentId}. ${deletedCount} comments deleted${
+                      deletedCount === 0
+                          ? `.`
+                          : ` (comment was found in initial query but may have been deleted by another source prior to deletion by this process).`
+                  }`
+                : `comment successfully deleted at: ${commentId}. ${deletedCount} comments deleted.`
+        }
+    `);
 });
