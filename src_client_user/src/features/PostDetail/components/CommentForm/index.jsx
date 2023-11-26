2bb938bf8435187f69508f070790f408ba04e630
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-26 14:31:02 +0000

    Add handler func props and close form button to CommentForm

--- src_client_user/src/features/PostDetail/components/CommentForm/index.jsx
+++ src_client_user/src/features/PostDetail/components/CommentForm/index.jsx
@@ -1,8 +1,34 @@
-const CommentForm = () => {
+import PropTypes from "prop-types";
+import styles from "./index.module.css";
+
+import MaterialSymbolsButton from "@/components/MaterialSymbolsButton";
+
+const CommentForm = ({
+    onCloseHandler,
+    onSubmitHandler,
+}) => {
     return (
-        <>
-        </>
+        <div className={styles["wrapper"]}>
+        <div className={styles["container"]}>
+            <MaterialSymbolsButton
+                aria-label="Close form"
+                text="close"
+                onClickHandler={onCloseHandler}
+                sizeRem={1.8}
+            />
+        </div>
+        </div>
     );
+}
+
+CommentForm.propTypes = {
+    onCloseHandler: PropTypes.func,
+    onSubmitHandler: PropTypes.func,
+}
+
+CommentForm.defaultProps = {
+    onCloseHandler: () => {},
+    onSubmitHandler: () => {},
 }
 
 export default CommentForm;
