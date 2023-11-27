c3847fe989c205724416624260f919f14fcb6433
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-27 21:08:49 +0000

    Add submit button and requirement message to CommentForm

--- src_client_user/src/features/PostDetail/components/CommentForm/index.jsx
+++ src_client_user/src/features/PostDetail/components/CommentForm/index.jsx
@@ -10,28 +10,42 @@ const CommentForm = ({
     return (
         <div className={styles["wrapper"]}>
         <div className={styles["container"]}>
-            <div className={styles["close-form-button"]}>
-                <MaterialSymbolsButton
-                    aria-label="Close form"
-                    text="close"
-                    onClickHandler={onCloseHandler}
-                    sizeRem={1.8}
-                />
+            <div className={styles["top-row"]}>
+                <div className={styles["close-form-button"]}>
+                    <MaterialSymbolsButton
+                        aria-label="Close form"
+                        text="close"
+                        onClickHandler={onCloseHandler}
+                        sizeRem={1.8}
+                    />
+                </div>
+                <h4
+                    className={styles["requirement-message"]}
+                >
+                        All fields marked with a * are <strong>required</strong>.
+                </h4>
             </div>
             <form
                 className={styles["form"]}
                 method="POST"
                 action="/"
             >
-                <label className={styles["form-label"]}>First Name *
-                    <input type="text" id="first-name" name="first_name"></input>
-                </label>
-                <label className={styles["form-label"]}>Last Name *
-                    <input type="text" id="last-name" name="last_name"></input>
-                </label>
-                <label className={styles["form-label"]}>Your Comment *
+                <div className={styles["names"]}>
+                    <label className={styles["first-name"]}>First Name *
+                        <input type="text" id="first-name" name="first_name"></input>
+                    </label>
+                    <label className={styles["last-name"]}>Last Name *
+                        <input type="text" id="last-name" name="last_name"></input>
+                    </label>
+                </div>
+                <label className={styles["comment"]}>Your Comment *
                     <textarea id="text" name="text"></textarea>
                 </label>
+                <button
+                    className={styles["submit-button"]}
+                    type="submit"
+                    onClick={onSubmitHandler}
+                >Post Comment</button>
             </form>
         </div>
         </div>
