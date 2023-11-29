8c3f0b9cfca054f66d3063066284597b162eff9c
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-29 22:26:54 +0000

    Set up form fields for LoginForm component

--- src_client_admin/src/features/LoginForm/index.jsx
+++ src_client_admin/src/features/LoginForm/index.jsx
@@ -1,9 +1,52 @@
+import { useState } from "react";
 import styles from "./index.module.css";
 
 const LoginForm = () => {
+    const [submissionErrors, setSubmissionErrors] = useState([]);
+
+    const login = (e) => {}
+
     return (
-        <>
-        </>
+        <div className={styles["wrapper"]}>
+        <div className={styles["container"]}>
+            <form
+                className={styles["form"]}
+                method="POST"
+                action=""
+            >
+                <div className={styles["form-fields"]}>
+                    <label className={styles["username"]}>First Name *
+                        <input type="text" id="username" name="username" required></input>
+                    </label>
+                    <label className={styles["password"]}>Last Name *
+                        <input type="password" id="password" name="password" required></input>
+                    </label>
+                </div>
+                <button
+                    className={styles["submit-button"]}
+                    type="submit"
+                    onClick={(e) => {
+                        login(e);
+                        e.currentTarget.blur();
+                        e.preventDefault();
+                    }}
+                    onMouseLeave={(e) => {
+                        e.currentTarget.blur();
+                    }}
+                >Log in</button>
+                {submissionErrors.length > 0
+                ?   <div className={styles["submission-errors"]}>
+                        <h4 className={styles["error-title"]}>Error(s):</h4>
+                        <ul className={styles["error-list"]}>
+                            {submissionErrors.map((error) => {
+                                return <li className={styles["error-item"]}>{error}</li>
+                            })}
+                        </ul>
+                    </div>
+                :   null}
+            </form>
+        </div>
+        </div>
     );
 }
 
